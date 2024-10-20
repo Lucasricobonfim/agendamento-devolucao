53c65f50c81ba21b5c81f7627f681e81b5c8475a
Lucas <lucasgabrielsilvageh@gmail.com>
2024-10-20 00:07:29 -0300

    feat: Sidebar responsiva

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -48,152 +48,70 @@ if (!isset($_SESSION['token'])) {
     <link rel="stylesheet" href="<?= $base; ?>/css/tabela/tabela-responsive.css">
     <!-- MaskPlugin -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-
-    <style>
-
-        .sidebar {
-            width: 250px;
-            background-color: #f8f9fa;
-            padding: 20px;
-            height: 100vh;
-            position: fixed;
-            left: 0; 
-            transition: left 0.3s ease;
-            z-index: 2;
-        }
-
-
-        .header i {
-            font-size: 22px;
-        }
-
-
-        .sidebar.open {
-            left: 0;
-        }
-
-        .sidebar img {
-            width: 100%;
-            margin-bottom: 20px;
-        }
-
-        .sidebar ul {
-            list-style: none;
-            padding: 0;
-        }
-
-        .sidebar ul li {
-            margin-bottom: 20px;
-        }
-
-        .sidebar ul li a {
-            text-decoration: none;
-            color: #000;
-            font-size: 18px;
-            display: flex;
-            align-items: center;
-        }
-
-        .sidebar ul li a i {
-            margin-right: 10px;
-            font-size: 22px;
-        }
-        .hamburger {
-            font-size: 24px;
-            cursor: pointer;
-        }
-    
-        body.sidebar-open {
-            margin-left: 250px; 
-        }
-
-        .header.sidebar-open {
-            margin-left: 250px; 
-        }
-
-       
-        .content {
-            margin-top: 50px; 
-            transition: margin-left 0.3s ease; 
-        }
-
-        .content.sidebar-open {
-            margin-left: 250px; 
-        }
-
-        @media screen and (max-width: 1425px) {
-
-            .sair-icon{
-                    margin-right: 280px;
-            }
-        }
-
-        
-    </style>
-
 </head>
 
 <body style="background-color: #EDF2F6;">
+    <div class="sair-icon" style="display: flex; align-items: center;">
+        <i class="fa-solid fa-user"></i>
+        <span style="margin-left: 5px;"><?= $_SESSION['usuario'] ?></span>
+        <div style="border-left: 1px solid #ccc; height: 20px; margin: 0 10px;"></div>
+
+        <a href="<?= $base; ?>/deslogar" style="color: rgb(161, 161, 170); margin-left: 7px;">
+            <i class="fas fa-sign-out-alt"></i>
+        </a>
+    </div>
     <header class="header">
         <div>
             <!-- Ícone de menu hamburger -->
             <i class="fas fa-bars hamburger" style="font-size: 22px;"></i>
         </div>
-        <div class="sair-icon" style="display: flex; align-items: center;">
-            <i class="fa-solid fa-user"></i> 
-            <span style="margin-left: 5px;"><?= $_SESSION['usuario'] ?></span>
-            <div style="border-left: 1px solid #ccc; height: 20px; margin: 0 10px;"></div>
-
-            <a href="<?= $base; ?>/deslogar" style="color: rgb(161, 161, 170); margin-left: 7px;">
-                <i class="fas fa-sign-out-alt"></i>
-            </a>
-        </div>
     </header>
     <aside class="sidebar">
         <img src="<?= $base; ?>/img/logo_topo.png" class="img-fluid" alt="texte">
+        <hr>
         <ul>
             <li><a class="negrito" href="<?= $base; ?>/inicio"><i class="fa-solid fa-house" style="margin-right: 10px; font-size: 22px;"></i>Inicio</a></li>
-            <hr style="margin: 0;">
+            <hr>
             <?php if ($_SESSION['idgrupo'] == 1) { ?>
                 <li><a class="negrito" href="<?= $base; ?>/usuario"><i class="fa-solid fa-users-gear" style="margin-right: 10px; font-size: 22px;"></i>Manutenção de Usuários</a></li>
             <?php } ?>
-            <hr style="margin: 0;">
+            <hr>
             <?php if ($_SESSION['idgrupo'] == 1) { ?>
                 <li><a class="negrito" href="<?= $base; ?>/transportadoras"><i class="fa-solid fa-truck" style="margin-right: 10px; font-size: 22px;"></i>Transportadoras</a></li>
             <?php } ?>
-            <hr style="margin: 0;">
+            <hr>
             <?php if ($_SESSION['idgrupo'] == 1) { ?>
                 <li><a class="negrito" href="<?= $base; ?>/centro-distribuicao"><i class="fa-solid fa-arrow-right-to-bracket" style="margin-right: 10px; font-size: 22px;"></i>Centro de distribuição</a></li>
             <?php } ?>
-            <hr style="margin: 0;">
+            <hr>
             <li><a class="negrito" href="<?= $base; ?>/agendamento"><i class="fa-solid fa-clipboard-list" style="margin-right: 10px; font-size: 22px;"></i>Agendar</a></li>
-            <hr style="margin: 0;">
+            <hr>
             <li><a class="negrito" href="<?= $base; ?>/agendamento-listar"><i class="fa-solid fa-list-check" style="margin-right: 10px; font-size: 22px;"></i>Listar Agendamentos</a></li>
-            <hr style="margin: 0;">
+            <hr>
             <li><a class="negrito" href="<?= $base; ?>/solicitacoes"><i class="fa-solid fa-list-check" style="margin-right: 10px; font-size: 22px;"></i>Solicitações</a></li>
-            <hr style="margin: 0;">
+            <hr>
         </ul>
     </aside>
-<script>
-    //   $('.hamburger').on('click', function () {
-    //         $('.sidebar').toggleClass('open');
-    //         $('body').toggleClass('sidebar-open'); 
-    //         $('.header').toggleClass('sidebar-open'); 
-    //       });
+    <script>
+        //   $('.hamburger').on('click', function () {
+        //         $('.sidebar').toggleClass('open');
+        //         $('body').toggleClass('sidebar-open'); 
+        //         $('.header').toggleClass('sidebar-open'); 
+        //       });
 
-        $('.hamburger').on('click', function () {
-        $('.sidebar').toggleClass('open');
-        $('body').toggleClass('sidebar-open');
-        $('.header').toggleClass('sidebar-open');
+        $('.hamburger').on('click', function() {
+            $('.sidebar').toggleClass('open');
+            $('body').toggleClass('sidebar-open');
+            $('.header').toggleClass('sidebar-open');
 
-           if ($('.sidebar').hasClass('open')) {
-            $('.sidebar').css('left', '0');
-            $('body').css('margin-left', '250px');
-            $('.header').css('margin-left', '250px');
-        } else {
-            $('.sidebar').css('left', '-250px');
-            $('body').css('margin-left', '0');
-            $('.header').css('margin-left', '0');
-        }
-});
-</script>
+            if ($('.sidebar').hasClass('open')) {
+                $('.sidebar').css('left', '0');
+                $('body').css('margin-left', '250px');
+                $('.header').css('margin-left', '250px');
+            } else {
+                $('.sidebar').css('left', '-250px');
+                $('body').css('margin-left', '0');
+                $('.header').css('margin-left', '0');
+            }
+        });
+    </script>
